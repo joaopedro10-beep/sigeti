d5fd98a1b795237b7a317b2c74ebf321ef1e98f9
João Pedro Machado de Oliveira <joaopedromachadodeoliveira10@gmail.com>
2026-05-11 16:21:15 -0300

    refactor(controller): atualizar Technician\\DashboardController para requirePermission

--- app/Controllers/Technician/DashboardController.php
+++ app/Controllers/Technician/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Technician;
 
 use App\Core\Auth;
 use App\Core\Controller;
+use App\Core\Permission;
 use App\Models\Ticket\Ticket;
 use App\Models\User;
 
@@ -12,12 +13,14 @@ class DashboardController extends Controller
     public function __construct()
     {
         parent::__construct("App");
-        Auth::requireRole(User::TECHNICIAN);
+        Auth::requirePermission(Permission::VIEW_TECHNICIAN_DASHBOARD);
 
     }
 
     public function index()
     {
+        Auth::requirePermission(Permission::VIEW_REQUESTER_DASHBOARD);
+
         $ticketsModel = new Ticket();
         $tickets = (new Ticket())->ticketsOrderedByStatusPriorityAndOpeningDate();
         $quantityTicketsByMonth = $ticketsModel->countTicketsByMonth();

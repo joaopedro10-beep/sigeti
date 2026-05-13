b3ba52d932c9c6308281bb2724b5a83b0753577f
João Pedro Machado de Oliveira <joaopedromachadodeoliveira10@gmail.com>
2026-05-13 15:41:48 -0300

    feat(controller): adicionar método index ao RoleController

--- app/Controllers/Admin/RoleController.php
+++ app/Controllers/Admin/RoleController.php
@@ -2,9 +2,29 @@
 
 namespace App\Controllers\Admin;
 
+use App\Core\Auth;
 use App\Core\Controller;
+use App\Core\Permission;
+use App\Models\Role\Role;
 
 class RoleController extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct("App");
+        Auth::requirePermission(Permission::VIEW_ROLES);
+    }
 
+    public function index():void
+    {
+        Auth::requirePermission(Permission::VIEW_ROLES);
+
+        $roles = (new Role())->orderBy("name", "ASC")->get();
+
+        echo $this->view->render("admin/role/index", [
+            "roles" => $roles
+        ]);
+
+      clear_old();
+    }
 }
